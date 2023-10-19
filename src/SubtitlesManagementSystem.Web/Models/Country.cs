8397e10c3127c2283540e99d0e0d27ab4fa40311
plamenna.petrova <plamennavp@abv.bg>
2023-10-19 09:09:04 +0300

    Add project files.

---
+++ src/SubtitlesManagementSystem.Web/Models/Country.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SubtitlesManagementSystem.Web.Models
+{
+    public class Country
+    {
+        public Country()
+        {
+            Id = Guid.NewGuid().ToString().Substring(0, 7);
+        }
+
+        [Key]
+        public string Id { get; set; }
+
+        [Required]  
+        public string Name { get; set; }
+    }
+}
