2626c4a26847e4a51fe3335f4bd396785d561f63
plamenna.petrova <plamennavp@abv.bg>
2024-01-15 20:00:37 +0200

    Added sample xUnit and Moq unit tests for services and controllers

---
+++ src/SubtitlesManagementSystem.Tests/Services/ActorServiceTests.cs
@@ -0,0 +1,97 @@
+﻿using Data.DataAccess.Repositories.Interfaces;
+using Data.DataModels.Entities;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using SubtitlesManagementSystem.Business.Services.Actors;
+using SubtitlesManagementSystem.Web.Models.Actors.BindingModels;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace SubtitlesManagementSystem.Tests.Services
+{
+    public class ActorServiceTests
+    {
+        private readonly Mock<IActorRepository> _actorRepositoryMock;
+
+        private readonly Mock<IFilmProductionRepository> _filmProductionRepositoryMock;
+
+        private readonly Mock<IFilmProductionActorRepository> _filmProductionActorRepositoryMock;
+
+        private readonly IActorService _actorService;
+
+        public ActorServiceTests()
+        {
+            _actorRepositoryMock = new Mock<IActorRepository>();
+            _filmProductionRepositoryMock = new Mock<IFilmProductionRepository>();
+            _filmProductionActorRepositoryMock = new Mock<IFilmProductionActorRepository>();
+
+            _actorService = new ActorService(
+                _actorRepositoryMock.Object,
+                _filmProductionRepositoryMock.Object,
+                _filmProductionActorRepositoryMock.Object
+            );
+        }
+
+        [Fact]
+        public void Test_AddActorEntity_IsVerifiable()
+        {
+            _actorRepositoryMock.Setup(arm => arm.Add(It.IsAny<Actor>())).Verifiable();
+        }
+
+        [Fact]
+        public void Test_AddFilmProductionEntity_IsVerifiable()
+        {
+            _filmProductionRepositoryMock.Setup(fprm => fprm.Add(It.IsAny<FilmProduction>())).Verifiable();
+        }
+
+        [Fact]
+        public void Test_AddFilmProductionActorEntity_IsVerifiable()
+        {
+            _filmProductionActorRepositoryMock.Setup(fparp => fparp.Add(It.IsAny<FilmProductionActor>())).Verifiable();
+        }
+
+        [Fact]
+        public void Test_ActorRepositoryGetById_ShouldReturnActor()
+        {
+            _actorRepositoryMock.Setup(arm => arm.GetById(It.IsAny<string>())).Returns(new Actor());
+        }
+
+        [Theory]
+        [InlineData("Christian", "Bale", new string[] { "1aab45d", "45d27d5", "45d27d5" })]
+        [InlineData("Elliot", "Page", new string[] { "45d27d5", "45d27d5" })]
+        [InlineData("Joseph", "Gordon-Levitt", new string[] { "1aab45d" })]
+        [InlineData("Anne", "Hathaway", new string[] { "587bcfe", "1aab45d" })]
+        [InlineData("Franka", "Potente", null)]
+        [InlineData("Matthew", "McConaughey", null)]
+        [InlineData("Jessica", "Chastain", null)]
+        [InlineData("Matt", "Damon", null)]
+        [InlineData("Leonardo", "Di Caprio", null)]
+        [InlineData("Cillian", "Murphy", null)]
+        [InlineData("Natalie", "Portman", null)]
+        [InlineData("Hugo", "Weaving", null)]
+        public void Test_CreateActor_ShouldReturnTrue(string firstName, string lastName, string[] selectedFilmProductions)
+        {
+            // Arrange
+            var testActorToCreate = new CreateActorBindingModel
+            {
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            var genericIdentity = new GenericIdentity("admin");
+
+            var claimsPrincipal = new ClaimsPrincipal(genericIdentity);
+
+            var defaultHttpContext = new DefaultHttpContext
+            {
+                User = claimsPrincipal,
+            };
+
+            // Act
+            bool actorCreationActualResult = _actorService.CreateActor(testActorToCreate, selectedFilmProductions, defaultHttpContext.User.Identity.Name);
+
+            // Assert
+            Assert.True(actorCreationActualResult);
+        }
+    }
+}
---
+++ src/SubtitlesManagementSystem.Tests/Web/ActorsControllerTests.cs
@@ -0,0 +1,101 @@
+﻿using Data.DataAccess.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SubtitlesManagementSystem.Business.Services.Actors;
+using SubtitlesManagementSystem.Business.Transactions.Interfaces;
+using SubtitlesManagementSystem.Common.Helpers;
+using SubtitlesManagementSystem.Web.Controllers;
+using SubtitlesManagementSystem.Web.Models.Actors.ViewModels;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace SubtitlesManagementSystem.Tests.Web
+{
+    public class ActorsControllerTests
+    {
+        private readonly Mock<IActorService> _actorServiceMock;
+
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+
+        private readonly ActorsController _actorsController;
+
+        public ActorsControllerTests()
+        {
+            _actorServiceMock = new Mock<IActorService>();
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+
+            var genericIdentity = new GenericIdentity("admin");
+
+            var claimsPrincipal = new ClaimsPrincipal(genericIdentity);
+
+            var defaultHttpContext = new DefaultHttpContext
+            {
+                User = claimsPrincipal,
+            };
+
+            var controllerContext = new ControllerContext
+            {
+                HttpContext = defaultHttpContext
+            };
+
+            _actorsController = new ActorsController(_actorServiceMock.Object, _unitOfWorkMock.Object)
+            {
+                ControllerContext = controllerContext
+            };
+        }
+
+        [Fact]
+        public void Test_ActorsController_ShouldHaveValidType()
+        {
+            Assert.IsType<ActorsController>(_actorsController);
+        }
+
+        [Fact]  
+        public void Test_ActorsControllerIndexAction_ShouldReturnValidSortingFilteringAndPaginationData()
+        {
+            // Arrange
+            int expectedPagesCount = 1;
+            int expectedCurrentPage = 1;
+            int expectedActorsCount = 1;
+
+            var testActors = new List<AllActorsViewModel>
+            {
+                new AllActorsViewModel
+                {
+                    FirstName = "Christian",
+                    LastName = "Bale"
+                },
+                new AllActorsViewModel
+                {
+                    FirstName = "Elliot",
+                    LastName = "Page"
+                },
+                new AllActorsViewModel
+                {
+                    FirstName = "Joseph",
+                    LastName = "Gordon-Levitt"
+                }
+            };
+
+            // Act
+            _actorServiceMock.Setup(asm => asm.GetAllActors()).Returns(testActors);
+
+            var sortedFilteredAndPaginatedActorsActionResult = _actorsController.Index("actor_first_name_descending", "Jo", "Jo", 3, null);
+            var sortedFilteredAndPaginatedActionViewResult = Assert.IsType<ViewResult>(sortedFilteredAndPaginatedActorsActionResult);
+            var allActorsViewModelPaginatedList = Assert.IsAssignableFrom<PaginatedList<AllActorsViewModel>>(
+                sortedFilteredAndPaginatedActionViewResult.ViewData.Model
+            );
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.Equal(expected: "Joseph", actual: allActorsViewModelPaginatedList[0].FirstName);
+                Assert.Equal(expected: "Gordon-Levitt", actual: allActorsViewModelPaginatedList[0].LastName);
+                Assert.Equal(expectedActorsCount, allActorsViewModelPaginatedList.Count);
+                Assert.Equal(expectedCurrentPage, allActorsViewModelPaginatedList.PageIndex);
+                Assert.Equal(expectedPagesCount, allActorsViewModelPaginatedList.TotalPages);
+            });
+        }
+    }
+}
